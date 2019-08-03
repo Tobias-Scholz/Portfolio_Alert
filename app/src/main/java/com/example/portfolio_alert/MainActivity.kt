4e6f370b5801758dfe4395496b8058dbb7a2ebf3
Tobeyyyyy <tobiasscholz2000@gmail.com>
2019-08-03 14:59:07 +0200

    First Commit

---
+++ app/src/main/java/com/example/portfolio_alert/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.portfolio_alert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            d("Hallo", " Hallo2")
+        }
+    }
+}
