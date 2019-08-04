e3838c40602e812425dd052faba5bd19c0e943dd
Tobeyyyyy <tobiasscholz2000@gmail.com>
2019-08-04 19:55:44 +0200

    First Commit

--- app/src/main/java/com/example/portfolio_alert/MainActivity.kt
+++ app/src/main/java/com/example/portfolio_alert/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.portfolio_alert
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
 import kotlinx.android.synthetic.main.activity_main.*
+import android.net.NetworkInfo
+import android.content.Context.CONNECTIVITY_SERVICE
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +20,11 @@ class MainActivity : AppCompatActivity() {
         button1.setOnClickListener {
             d("Hallo", " Hallo2")
         }
+
+        InternetCheck(object : InternetCheck.Consumer {
+            override fun accept(internet: Boolean?) {
+                Log.d("test", "asdasdas")
+            }
+        })
     }
 }
