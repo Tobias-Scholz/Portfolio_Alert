74b302110bd588ae30fc5b55c606939e87143558
Tobeyyyyy <tobiasscholz2000@gmail.com>
2019-08-10 14:30:36 +0200

    Bugfixes

--- app/src/main/java/com/example/portfolio_alert/MainActivity.kt
+++ app/src/main/java/com/example/portfolio_alert/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.Button
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -20,11 +22,11 @@ class MainActivity : AppCompatActivity() {
     val stock_list : ArrayList<Stock> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         stock_list.add(Stock("Microsoft", "msf.de"))
         stock_list.add(Stock("SAP", "sap.de"))
 
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
@@ -35,6 +37,8 @@ class MainActivity : AppCompatActivity() {
             URLEncoder.encode("msf.de", StandardCharsets.UTF_8.name())))
         d("Tobias", "2")
 
+        rv_stock_list.layoutManager = LinearLayoutManager(this)
+        rv_stock_list.adapter = StockAdapter(stock_list, this)
     }
 
     fun <ViewT : View> Activity.bindView(@IdRes idRes: Int): Lazy<ViewT> {
